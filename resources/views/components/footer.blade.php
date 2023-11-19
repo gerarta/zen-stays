1acccf5bbf3a16299fa071d8db6ab8433da43327
geren <gerenwidiarta477@gmail.com>
2023-11-20 00:04:42 +0700

    feat:added landing page

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,30 @@
+<footer class="mt-8 bg-[#7EAA92] top-0 left-0 right-0 p-4" >
+  <div class="grid divide-x-2 gap-4  grid-cols-3 content-center w-[85%] md:w-1/2 mx-auto">
+    <div class="shrink">
+        <img src="/images/logo-white.png" class="object-cover object-center w-full" alt="">
+    </div>
+    <div class="flex items-center justify-center flex-col">
+        <h1 class="text-2xl mb-4 text-white font-bold justify-center">Follow Us </h1>
+        <div class="grid grid-cols-4 mx-2 gap-1">
+            <a href="" class="bg-[#FFD9B7] border rounded-full p-2">
+                <img src="/images/instagram.png" alt="instagram" class=" object-center object-cover block mx-auto">
+            </a>
+            <a href="" class="bg-[#FFD9B7] border rounded-full p-2">
+                <img src="/images/whatsapp.png" alt="whatsapp" class=" object-center object-cover block mx-auto">
+            </a>
+            <a href="" class="bg-[#FFD9B7] border rounded-full p-2">
+                <img src="/images/email.png" alt="email" class=" object-center object-cover block mx-auto">
+            </a>
+            <a href="" class="bg-[#FFD9B7] border rounded-full p-2 flex items-center justify-center">
+                <img src="/images/youtube.png" alt="youtube" class="  object-center object-cover">
+            </a>
+        </div>
+        <p class="text-white ms-2 text-sm md:text-xl font-bold mt-3"> © 2023. All Rights Reserved. </p>
+    </div>
+    <div class="flex flex-col justify-center items-center">
+        <h1 class="text-2xl font-bold text-white">Kebijakan </h1>
+        <p class="font-bold text-[#FFD9B7]"> Kebijakan Privasi</p>
+        <p class="font-bold text-[#FFD9B7]"> Syarat & Ketentuan</p>
+    </div>
+  </div>
+</footer>
